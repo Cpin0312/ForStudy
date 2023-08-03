3dfb7c6d7e8195f3a86aeb95eb7ff9308bbe601d
Sakura <ohcpin@gmail.com>
2023-08-03 23:42:56 +0900

    Latest

--- 03_Sakura/menu/myMenu.js
+++ 03_Sakura/menu/myMenu.js
@@ -13,5 +13,13 @@ switch(selectedItem){
     Editor.SelectAll(0);
     Editor.ReplaceAll('^[\\r\\n]+', '', 772);
     Editor.ReDraw(0);
+  case indexMacro + 1 :
+  	var regexWork = "^((?!@REPLACE@).)*$";
+  	var remain = InputBox("Remain Word","");
+  	regexWork = regexWork.replace("@REPLACE@", remain);
+    Editor.ReplaceAll(regexWork, '', 772);
+    Editor.ReDraw(0);
 
 }
+
+Editor.GoFiletop(0);
